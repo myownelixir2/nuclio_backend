4874e28ad66a4bf0f18ab9a1c150ccb1cf0c92e7
myownelixir2 <wojciech.bednarz@protonmail.com>
2022-10-15 23:57:06 +0100

    feature - adding post fx and mixer module

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
 from app.main import app
+
+
+
+
+import glob
+
+random_id = 'lbydrfmsucta'
+
+current_sequences_list = glob.glob(f'temp/mixdown_{random_id}_*.mp3')
+
+'-i '.join(current_sequences_list)
+
+'-i ' + ' -i '.join(current_sequences_list)
+ "ffmpeg -y -i FILE1 -i FILE2 -filter_complex '[0:0][1:0] amix=inputs=2:duration=longest' -c:a libmp3lame OUTPUTFILE"
