489def09a1ea9027b9c73c9467a0741d3f8b105b
myownelixir2 <wojciech.bednarz@protonmail.com>
2023-05-15 22:47:22 +0100

    refactorin storage.py

--- app/storage/storage.py
+++ app/storage/storage.py
@@ -1,10 +1,12 @@
 import io
 import zipfile
-from botocore.exceptions import ClientError
+import logging
+from botocore.exceptions import BotoCoreError, ClientError
 import os
 import numpy as np
 import pandas as pd
 from pydantic import Field, BaseSettings, validator
+from typing import Any
 import boto3
 import pydub
 from typing import List
@@ -15,13 +17,38 @@ import random
 from app.utils.utils import JobTypeValidator
 
 class StorageCreds(BaseSettings):
+    """
+    A class used to manage and validate storage credentials.
+
+    Attributes:
+    -----------
+    endpoint_url : str
+        The URL of the storage endpoint.
+    access_key_id : str
+        The access key ID for the storage.
+    secret_access_key : str
+        The secret access key for the storage.
+    """
     endpoint_url: str = Field(..., env="STORAGE_URL")
     access_key_id: str = Field(..., env="STORAGE_KEY")
     secret_access_key: str = Field(..., env="STORAGE_SECRET")
    
 
     @validator("endpoint_url", "access_key_id", "secret_access_key")
-    def creds_validator(cls, v):
+    def creds_validator(cls, v: Any) -> Any:
+        """
+        Validates the provided storage credentials.
+
+        Parameters:
+        -----------
+        v : Any
+            The value of the attribute being validated.
+
+        Returns:
+        --------
+        Any
+            The validated value.
+        """
 
         if v is None:
             raise ValueError(
@@ -29,24 +56,42 @@ class StorageCreds(BaseSettings):
             )
         return v
 
+import logging
+from botocore.exceptions import BotoCoreError, ClientError
 
 class StorageEngine:
+    """
+    Class to manage storage operations with an S3-compatible storage system.
+
+    Attributes:
+    -----------
+    job_config : object
+        Configuration settings for the job.
+    asset_type : str
+        Type of the asset.
+    client : object
+        Boto3 S3 resource object.
+    """
+
     def __init__(self, job_config, asset_type):
         self.job_config = job_config
         self.asset_type = asset_type
+        self.client = self.client_init()
+        self.logger = logging.getLogger(__name__)
 
     def client_init(self):
+        """Initialize S3 client using storage credentials."""
         try:
-            self.client = boto3.resource(
+            client = boto3.resource(
                 "s3",
                 endpoint_url=StorageCreds().endpoint_url,
                 aws_access_key_id=StorageCreds().access_key_id,
                 aws_secret_access_key=StorageCreds().secret_access_key,
             )
-            return self.client
-        except Exception as e:
-            print(e)
-            return True
+            return client
+        except (BotoCoreError, ClientError) as e:
+            self.logger.error(f"Error initializing S3 client: {e}")
+            raise e
 
     def __resolve_type(self):
         job_paths = self.job_config.path_resolver()
@@ -89,91 +134,147 @@ class StorageEngine:
             }
             return d_paths
 
-    def get_object(self):
+    def get_object(self, bucket_name="sample-dump"):
+        """Download file from S3 to local."""
         try:
-            client = self.client_init()
-            bucket = client.Bucket("sample-dump")
+            bucket = self.client.Bucket(bucket_name)
             _type = self.__resolve_type()
             bucket.download_file(_type["cloud_path"], _type["local_path"])
             return True
-        except Exception as e:
-            print(e)
-            return False
+        except (BotoCoreError, ClientError) as e:
+            self.logger.error(f"Error getting object from S3: {e}")
+            raise e
 
     def delete_local_object(self):
+        """Delete local file."""
         try:
             _type = self.__resolve_type()
             os.remove(_type["local_path"])
             return True
-        except Exception as e:
-            print(e)
-            return False
-    
-    
-    def upload_object_local(self, local_path, cloud_path):
+        except OSError as e:
+            self.logger.error(f"Error deleting local object: {e}")
+            raise e
+
+    def upload_object_local(self, local_path, cloud_path, bucket_name="sample-dump"):
+        """Upload local file to S3."""
         try:
-            client = self.client_init()
-            bucket = client.Bucket("sample-dump")
+            bucket = self.client.Bucket(bucket_name)
             bucket.upload_file(local_path, cloud_path)
             return True
-        except Exception as e:
-            print(e)
-            return False
+        except (BotoCoreError, ClientError) as e:
+            self.logger.error(f"Error uploading local object to S3: {e}")
+            raise e
 
-    def upload_object(self):
+    def upload_object(self, bucket_name="sample-dump"):
+        """Upload local file to S3 based on job config."""
         try:
-            client = self.client_init()
-            bucket = client.Bucket("sample-dump")
+            bucket = self.client.Bucket(bucket_name)
             _type = self.__resolve_type()
             bucket.upload_file(_type["local_path"], _type["cloud_path"])
             return True
-        except Exception as e:
-            print(e)
-            return False
+        except (BotoCoreError, ClientError) as e:
+            self.logger.error(f"Error uploading object to S3: {e}")
+            raise e
+
         
 class StoreEngineMultiFile:
+    """
+    This class handles the upload of multiple files to S3 storage.
+
+    Attributes:
+    -----------
+    job_id : str
+        Unique identifier for the job.
+    logger : object
+        Logger instance for logging status and error messages.
+    client : object
+        S3 client instance.
+
+    Methods:
+    --------
+    client_init():
+        Initializes and returns the S3 client.
+    upload_list_of_objects(files: List[str], bucket_path: str):
+        Uploads a list of files to a specified bucket path on S3.
+    """
     def __init__(self, job_id):
+        """
+        Initialize the S3 client.
+
+        Returns:
+        --------
+        boto3.resource
+            S3 client instance.
+        
+        Raises:
+        -------
+        Exception
+            If any error occurs during the client initialization.
+        """
         self.job_id: str = job_id
+        self.logger = logging.getLogger(__name__)
+        self.client = self.client_init()
 
     def client_init(self):
+        """Initialize the S3 client."""
         try:
-            self.client = boto3.resource(
+            client = boto3.resource(
                 "s3",
                 endpoint_url=StorageCreds().endpoint_url,
                 aws_access_key_id=StorageCreds().access_key_id,
                 aws_secret_access_key=StorageCreds().secret_access_key,
             )
-            return self.client
+            return client
         except Exception as e:
-            print(e)
-            return False
+            self.logger.error(f"Error initializing S3 client: {e}")
+            raise e
 
     def upload_list_of_objects(self, files: List[str], bucket_path: str) -> bool:
+        """
+        Uploads a list of files to a specified bucket path on S3.
+
+        Parameters:
+        -----------
+        files : List[str]
+            List of local file paths to upload.
+        bucket_path : str
+            The cloud bucket path to upload files.
+
+        Returns:
+        --------
+        bool
+            True if all files are uploaded successfully, False otherwise.
+
+        Raises:
+        -------
+        Exception
+            If any error occurs during the file upload.
+        """
         def sanitize_list(file_list: List[str]) -> List[str]:
             return [os.path.basename(file) for file in file_list]
 
         sanitized_files = sanitize_list(files)
         cloud_paths = [os.path.join(bucket_path, file) for file in sanitized_files]
-        status = True
+        
         try:
-            client = self.client_init()
-            bucket = client.Bucket("favs-dump")
-
-        except Exception as e:
-            print(f"An error occurred while initiating an s3 client: {e}")
-            status = False
-        else:
+            bucket = self.client.Bucket("favs-dump")
             for file, cloud_path in zip(files, cloud_paths):
                 try:
                     bucket.upload_file(file, cloud_path)
                 except Exception as e:
-                    print(
-                        f"An error occurred while uploading the file {file} to S3: {e}"
-                    )
-                    status = False
-        return status
+                    self.logger.error(f"Error uploading file {file} to S3: {e}")
+                    raise e
+            return True
+        except Exception as e:
+            self.logger.error(f"Error during S3 operations: {e}")
+            raise e
 
-class StorageEngineDownloader:
+import boto3
+import io
+import zipfile
+from botocore.exceptions import ClientError
+
+class StorageBase:
     def __init__(self, bucket):
         self.bucket = bucket
 
@@ -189,7 +290,7 @@ class StorageEngineDownloader:
         except Exception as e:
             print(e)
             return False
-        
+
     def client_init(self):
         try:
             self.client = boto3.client(
@@ -203,6 +304,41 @@ class StorageEngineDownloader:
             print(e)
             return False
 
+def handle_client_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ClientError as e:
+            print(e)
+            return None
+    return wrapper
+
+class StorageEngineDownloader(StorageBase):
+    """
+    This class represents a storage engine designed for downloading, manipulating, and uploading files 
+    using Amazon S3 storage. 
+
+    Attributes:
+        bucket (str): The name of the S3 bucket to use for storage operations.
+
+    Methods:
+        resource_init(): Initialize a boto3 resource object.
+        client_init(): Initialize a boto3 client object.
+        copy_objects(source_key, destination_key): Copy an object within the bucket.
+        download_in_memory_objects(key): Download an object from the bucket into memory.
+        create_arrangement_file(my_files, format): Create an audio file by concatenating multiple audio files.
+        upload_in_memory_object(output_file, in_memory_object): Upload an in-memory file to the bucket.
+        filter_objects(prefix_): Filter the objects in the bucket by a prefix.
+        generate_random_string(length): Generate a random string of a given length.
+        filter_files(file_list, suffix, mixdown_ids): Filter a list of files by suffix and mixdown id.
+        create_zip_file(my_files): Create a zip file from multiple files in the bucket.
+        get_presigned_url(file_name, expires_in): Generate a presigned URL for a file in the bucket.
+        upload_and_get_presigned_url(zip_name, in_memory_zip, expires_in): Upload a file to the bucket and generate a presigned URL for it.
+    """
+    def __init__(self, bucket):
+        super().__init__(bucket)
+
+    @handle_client_error
     def copy_objects(self, source_key: str, destination_key: str):
         client = self.client_init()
         client.copy_object(Bucket=self.bucket,
@@ -230,13 +366,10 @@ class StorageEngineDownloader:
         in_memory_arrangement.seek(0)
         return in_memory_arrangement
 
+    @handle_client_error
     def upload_in_memory_object(self, output_file: str, in_memory_object: io.BytesIO):
-        try:
-            client = self.client_init()
-            client.upload_fileobj(in_memory_object, self.bucket, output_file)
-        except ClientError as e:
-            print(e)
-            return None
+        client = self.client_init()
+        client.upload_fileobj(in_memory_object, self.bucket, output_file)
         return output_file
 
     def filter_objects(self, prefix_):
@@ -253,18 +386,11 @@ class StorageEngineDownloader:
         letters = string.ascii_lowercase
         return ''.join(random.choice(letters) for i in range(length))
 
-
     @staticmethod
     def filter_files(file_list, suffix, mixdown_ids):
-        filtered_list = []
-        for file in file_list:
-            if file.endswith(suffix):
-                if any(id_str in file for id_str in mixdown_ids):
-                    filtered_list.append(file)
-        return filtered_list
+        return [file for file in file_list if file.endswith(suffix) and any(id_str in file for id_str in mixdown_ids)]
 
     def create_zip_file(self, my_files):
-        #client = self.resource_init()
         client = self.client_init()
         in_memory_zip = io.BytesIO()
         with zipfile.ZipFile(in_memory_zip, mode="w", compression=zipfile.ZIP_DEFLATED) as archive:
@@ -274,35 +400,33 @@ class StorageEngineDownloader:
         in_memory_zip.seek(0)
         return in_memory_zip
 
+    @handle_client_error
     def get_presigned_url(self, file_name, expires_in=15):
         client = self.client_init()
-        try:
-            response = client.generate_presigned_url(
-                "get_object",
-                Params={"Bucket": self.bucket, "Key": file_name},
-                ExpiresIn=expires_in,
-            )
-        except ClientError as e:
-            print(e)
-            return None
+        response = client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": self.bucket, "Key": file_name},
+            ExpiresIn=expires_in,
+        )
         return response
 
+    @handle_client_error
     def upload_and_get_presigned_url(self, zip_name, in_memory_zip, expires_in=300):
         client = self.client_init()
         client.upload_fileobj(in_memory_zip, self.bucket, zip_name)
-        try:
-            response = client.generate_presigned_url(
-                "get_object",
-                Params={"Bucket": self.bucket, "Key": zip_name},
-                ExpiresIn=expires_in,
-            )
-        except ClientError as e:
-            print(e)
-            return None
+        response = client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": self.bucket, "Key": zip_name},
+            ExpiresIn=expires_in,
+        )
         return response
 
 
+
 class SnapshotManager:
+    """
+    This class represents a manager for handling snapshots in Amazon S3 storage.
+    """
     def __init__(self, bucket_name):
         self.bucket = bucket_name
         self.resource = self.resource_init()
@@ -384,9 +508,15 @@ class SnapshotManager:
             return False
 
     def generate_presigned_urls(self):
-        url_1 = self.client.generate_presigned_url('get_object', Params={'Bucket': 'snapshots-dump', 'Key': 'database_snapshot.csv'}, ExpiresIn=3600)
-        url_2 = self.client.generate_presigned_url('get_object', Params={'Bucket': 'snapshots-dump', 'Key': 'database_snapshot_files.csv'}, ExpiresIn=3600)
-        return url_1, url_2
-
+        """
+        Generate pre-signed URLs for database snapshot and snapshot files.
+        """
+        try:
+            url_1 = self.client.generate_presigned_url('get_object', Params={'Bucket': 'snapshots-dump', 'Key': 'database_snapshot.csv'}, ExpiresIn=3600)
+            url_2 = self.client.generate_presigned_url('get_object', Params={'Bucket': 'snapshots-dump', 'Key': 'database_snapshot_files.csv'}, ExpiresIn=3600)
+            return url_1, url_2
+        except ClientError as e:
+            self.logger.error(f"Error generating presigned urls: {e}")
+            raise e
 
 
